c289454100ff1ddb5a65832f998f63ad31926391
JongHyun94 <ant9406@naver.com>
2021-06-15 17:33:42 +0900

    [update] 접수 상세보기 화면구성

--- src/views/Register/RegisterRead.js
+++ src/views/Register/RegisterRead.js
@@ -1,52 +1,84 @@
 function RegisterRead(props) {
   return (
-    <div className="RegisterRead">
+    <div className="border">
       {/* 상단 메뉴 이름 */}
-      <div>
-        <h3>접수 상세 내역</h3>
+      <div className="RegisterRead_header">
+        <h4>접수 상세 내역</h4>
       </div>
       {/* 하단 내용 */}
-      <div>
+      <div className="RegisterRead_content">
         {/* 접수 상세 내역 내용 */}
-        <div>
+        <div className="RegisterRead_content_form">
           <form>
-            <div>
-              <label>환자명:</label>
-              <input type="text" value="민지현"/>
+            <div className="RegisterRead_content_list">
+              <div className="RegisterRead_content_list_label">
+                <h6>환자명:</h6>
+              </div>
+              <div className="RegisterRead_content_list_input">
+                <input type="text" value="민지현" />
+              </div>
             </div>
-            <div>
-              <label>생년월일:</label>
-              <input type="text" value="960119"/>
+            <div className="RegisterRead_content_list">
+              <div className="RegisterRead_content_list_label">
+                <h6>생년월일:</h6>
+              </div>
+              <div className="RegisterRead_content_list_input">
+                <input type="text" value="960119" />
+              </div>
             </div>
-            <div>
-              <label>전화번호:</label>
-              <input type="text" value="010-1234-5678"/>
+            <div className="RegisterRead_content_list">
+              <div className="RegisterRead_content_list_label">
+                <h6>전화번호:</h6>
+              </div>
+              <div className="RegisterRead_content_list_input">
+                <input type="text" value="010-1234-5678" />
+              </div>
             </div>
-            <div>
-              <label>담당의:</label>
-              <input type="text" value="김더존(D13801001001)"/>
+            <div className="RegisterRead_content_list">
+              <div className="RegisterRead_content_list_label">
+                <h6>담당의:</h6>
+              </div>
+              <div className="RegisterRead_content_list_input">
+                <input type="text" value="김더존(D13801001001)" />
+              </div>
             </div>
-            <div>
-              <label>진료 날짜:</label>
-              <input type="text" value="2021-06-03"/>
+            <div className="RegisterRead_content_list">
+              <div className="RegisterRead_content_list_label">
+                <h6>진료 날짜:</h6>
+              </div>
+              <div className="RegisterRead_content_list_input">
+                <input type="text" value="2021-06-03" />
+              </div>
             </div>
-            <div>
-              <label>진료 시간:</label>
-              <input type="text" value="14:30"/>
+            <div className="RegisterRead_content_list">
+              <div className="RegisterRead_content_list_label">
+                <h6>진료 시간:</h6>
+              </div>
+              <div className="RegisterRead_content_list_input">
+                <input type="text" value="14:30" />
+              </div>
             </div>
-            <div>
-              <label>접수 메모:</label>
-              <input type="text" value="복통"/>
+            <div className="RegisterRead_content_list">
+              <div className="RegisterRead_content_list_label">
+                <h6>접수 메모:</h6>
+              </div>
+              <div className="RegisterRead_content_list_input">
+                <input type="text" value="복통" />
+              </div>
             </div>
-            <div>
-              <label>의사소통 메모:</label>
-              <input type="text" value="10분 뒤에 들어갑니다."/>
+            <div className="RegisterRead_content_list">
+              <div className="RegisterRead_content_list_label">
+                <h6>의사소통 메모:</h6>
+              </div>
+              <div className="RegisterRead_content_list_input">
+                <input type="text" value="10분 뒤에 들어갑니다." />
+              </div>
             </div>
           </form>
         </div>
         {/* 수정 취소 버튼 */}
-        <div>
-          <button className="btn btn-primary btn-sm">수정</button>
+        <div className="RegisterRead_content_button">
+          <button className="btn btn-primary btn-sm mb-2">수정</button>
           <button className="btn btn-primary btn-sm">취소</button>
         </div>
       </div>
