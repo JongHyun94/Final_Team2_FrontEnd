7c8f2770982f16d29f5e0ad7940b5efaef3e2303
JongHyun94 <ant9406@naver.com>
2021-06-15 12:51:45 +0900

    [update] Register 인덱스 css 구성

--- src/views/Register/index.js
+++ src/views/Register/index.js
@@ -1,33 +1,45 @@
+import Switch from "react-router-dom";
+import RegisterCreateForm from "./RegisterCreateForm";
+import RegisterList from "./RegisterList";
+import RegisterPatientList from "./RegisterPatientList";
+import RegisterRead from "./RegisterRead";
+import RegisterTimeTable from "./RegisterTimeTable";
+import RegisterUpdateForm from "./RegisterUpdateForm";
+
 function Register(props) {
   return (
-    <>
+    <div className="Register">
     {/* 상단 */}
-      <div>
+      <div className="Register_1">
         {/* 접수 내역 */}
-        <div>
-            hi
+        <div className="RegisterList">
+          <RegisterList/>
         </div>
         {/* 접수 상세 내역 or  접수 수정*/}
-        <div>
-
+        <div className="RegisterRead">
+          <RegisterRead/>
+          {/* <RegisterUpdateForm/> */}
+          {/* <Switch>
+            <Route path="" exact component={}/>
+          </Switch> */}
         </div>
       </div>
       {/* 하단 */}
-      <div>
+      <div className="Register_2">
         {/* 환자 검색 */}
-        <div>
-
+        <div className="RegisterPatientList">
+          <RegisterPatientList/>
         </div>
         {/* 접수 등록 */}
-        <div>
-
+        <div className="RegisterCreateForm">
+          <RegisterCreateForm/>
         </div>
         {/* 접수 타임테이블 */}
-        <div>
-
+        <div className="RegisterTimeTable">
+          <RegisterTimeTable/>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 export default Register;
