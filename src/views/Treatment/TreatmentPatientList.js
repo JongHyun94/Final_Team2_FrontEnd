829a8fac9d7a4196c7676dd55a8d90d47ccd7460
ParkVitna <bbn960903@gmail.com>
2021-06-16 20:37:41 +0900

    [update] TreatmentPatientList 상태 추가

--- src/views/Treatment/TreatmentPatientList.js
+++ src/views/Treatment/TreatmentPatientList.js
@@ -1,106 +1,71 @@
+import { useState } from "react";
+import { AutoSizer, List } from "react-virtualized";
+
+function getBoards() {
+  const boards = [];
+  for (var i = 50; i >= 1; i--) {
+    boards.push({ b1: i, b2: "환자" + i, b3: "910111", b4: "F", b5: "메모" + i, b6: "대기" });
+  }
+  return boards;
+}
+
 function TreatmentPatientList(props) {
-    return(
+  const [boards, setBoards] = useState(getBoards);
 
-       <div>
-        <div className="TreatmentPatientList_title">
-        접수완료환자
-        </div>
-
-        <div className="TreatmentPatientList_border border">
-            
-            <div className="TreatmentPatientList_search">
-                <div className="row">
-                    <input type="date"/>
-                    <button className="button_team2_fill">이동</button>  
-
-                        <div className="row_1">대기:2명</div>
-                        <div className="row_2">완료:3명</div>
-                
-                </div>
-            </div>
+  const rowRenderer = ({index, key, style}) => {
+    return (
       
-        
-        <table className="table table-bordered TreatmentPatientList_table">
-            <thead className="TreatmentPatientList_table_thead">
-                <tr>
-                    <th></th>
-                    <th>접수 번호</th>
-                    <th>환자명</th>
-                    <th>생년월일</th>
-                    <th>성별</th>
-                    <th>의사소통 메모</th>
-                    <th>진료 상태</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>   
-                <td><input type="checkbox"/></td>
-                    <td>11111</td>
-                    <td>민지현</td>
-                    <td>910111</td>
-                    <td>M</td>
-                    <td></td>
-                    <td>대기</td>
-                </tr>
-                <tr>   
-                <td><input type="checkbox"/></td>
-                    <td>11111</td>
-                    <td>윤서영</td>
-                    <td>910111</td>
-                    <td>M</td>
-                    <td></td>
-                    <td>대기</td>
-                </tr>
-                <tr>   
-                <td><input type="checkbox"/></td>
-                    <td>11111</td>
-                    <td>박빛나</td>
-                    <td>910111</td>
-                    <td>F</td>
-                    <td></td>
-                    <td>대기</td>
-                </tr>
-                <tr>   
-                <td><input type="checkbox"/></td>
-                    <td>22222</td>
-                    <td>이종현</td>
-                    <td>910111</td>
-                    <td>F</td>
-                    <td></td>
-                    <td>대기</td>
-                </tr>
-                <tr>   
-                <td><input type="checkbox"/></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>   
-                <td><input type="checkbox"/></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>   
-                <td><input type="checkbox"/></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-               
-            </tbody>
-        </table>
-        </div>
-        </div> 
+        <tr key={key} style={style}>
+              <td key={boards.b1}>
+                <input type="checkbox" />
+              </td>
+              <td>{boards[index].b1}</td>
+              <td>{boards[index].b2}</td>
+              <td>{boards[index].b3}</td>
+              <td>{boards[index].b4}</td>
+              <td>{boards[index].b5}</td>
+              <td>{boards[index].b6}</td>
+        </tr>
     );
+  };
+
+  return (
+    <div>
+      <div className="TreatmentPatientList_title">접수완료환자</div>
+
+      <div className="TreatmentPatientList_border border">
+        <div className="TreatmentPatientList_search">
+          <div className="row">
+            <input type="date" />
+            <button className="button_team2_fill">이동</button>
+
+            <div className="row_1">대기:2명</div>
+            <div className="row_2">완료:3명</div>
+          </div>
+        </div>
+
+        <table className="table table-bordered TreatmentPatientList_table">
+          <thead className="TreatmentPatientList_table_thead">
+            <tr>
+              <th></th>
+              <th>접수 번호</th>
+              <th>환자명</th>
+              <th>생년월일</th>
+              <th>성별</th>
+              <th>의사소통 메모</th>
+              <th>진료 상태</th>
+            </tr>
+          </thead>
+          <tbody>
+            <AutoSizer disableHeight>
+              {({ width, height }) => {
+                return <List width={width} height={360} list={boards} rowCount={boards.length} rowHeight={44} rowRenderer={rowRenderer} overscanRowCount={5} />;
+              }}
+            </AutoSizer>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 export default TreatmentPatientList;
