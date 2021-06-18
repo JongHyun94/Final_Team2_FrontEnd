5ce16873954f8fb33b143d526f0c18b55e69aa9b
ParkVitna <bbn960903@gmail.com>
2021-06-18 16:20:15 +0900

    [update] Route

--- src/views/DataAnalysis/index.js
+++ src/views/DataAnalysis/index.js
@@ -2,8 +2,8 @@ import Data1 from "./Data1";
 import Data2 from "./Data2";
 import Data3 from "./Data3";
 import Data4 from "./Data4";
-import '@toast-ui/chart/dist/toastui-chart.min.css';
-import { BarChart, LineChart } from '@toast-ui/react-chart';
+// import '@toast-ui/chart/dist/toastui-chart.min.css';
+// import { BarChart, LineChart } from '@toast-ui/react-chart';
 
 function DataAnalysis(props) {
   return (
