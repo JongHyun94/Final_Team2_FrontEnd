5fa9292f69a9fb02cdf2b228eddbcfe2f2e1135c
ParkVitna <bbn960903@gmail.com>
2021-06-18 15:46:13 +0900

    [update] Inspection 01

--- src/views/Inspection/InspectionBarcodePop.js
+++ src/views/Inspection/InspectionBarcodePop.js
@@ -8,28 +8,28 @@ export const InspectionBarcodePop = (props) => {
     <div className={open ? 'openModal modal':'modal'}>
       {open? (
         <section>
-          <main>
             {/* InspectionBarcodePop */}
-          <div className="InspectionBarcodePop_1_1 border">
-            <img src={`/resources/img/${barcodeImg}`} width="100%" height="100%" alt=""></img>
-          </div>
-          <div className="InspectionBarcodePop_1_2 border">
-            <div>
-              <div className="mb-3">검사명 :</div>
-              <div className="mb-3">피검사자 :</div>
-              <div>검사자 :</div>
+          <div className="InspectionBarcodePop">
+            <div className="InspectionBarcodePop_1_1">
+              <img src={`/resources/img/${barcodeImg}`} width="100%" height="100%" alt=""></img>
             </div>
-            <div>
-              <div className="mb-3">{inspection_list_name}</div>
-              <div className="mb-3">{patient_name}</div>
-              <div>{inspection_inspector_name}</div>
+            <div className="InspectionBarcodePop_1_2 border">
+              <div className="mr-3 ml-3">
+                <div className="mb-3">검사명 :</div>
+                <div className="mb-3">피검사자 :</div>
+                <div>검사자 :</div>
+              </div>
+              <div>
+                <div className="mb-3">{inspection_list_name}</div>
+                <div className="mb-3">{patient_name}</div>
+                <div>{inspection_inspector_name}</div>
+              </div>
+            </div>
+            <div className="InspectionBarcodePop_1_3">
+              <button className="button_team2_fill" onClick={closeCheck}>확인</button>
+              <button className="button_team2_empty" onClick={closeCancel}>취소</button>
             </div>
           </div>
-          <div className="InspectionBarcodePop_1_3">
-            <button className="button_team2_fill" onClick={closeCheck}>확인</button>
-            <button className="button_team2_empty" onClick={closeCancel}>취소</button>
-          </div>
-          </main>
         </section>
       ):null}
     </div>
