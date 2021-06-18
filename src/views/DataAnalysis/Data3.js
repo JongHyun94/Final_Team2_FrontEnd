aeeb192ed813d641d5575d9c7b8bb6816f86d3c4
ParkVitna <bbn960903@gmail.com>
2021-06-18 16:13:45 +0900

    [add] 데이터분석

---
+++ src/views/DataAnalysis/Data3.js
@@ -0,0 +1,8 @@
+function Data3(props) {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Data3;
--- src/views/DataAnalysis/index.js
+++ src/views/DataAnalysis/index.js
@@ -1,6 +1,21 @@
+import Data1 from "./Data1";
+import Data2 from "./Data2";
+import Data3 from "./Data3";
+import Data4 from "./Data4";
+import '@toast-ui/chart/dist/toastui-chart.min.css';
+import { BarChart, LineChart } from '@toast-ui/react-chart';
+
 function DataAnalysis(props) {
   return (
     <>
+    <div>
+      <div><Data1/></div>
+      <div><Data2/></div>
+    </div>
+    <div>
+      <div><Data3/></div>
+      <div><Data4/></div>
+    </div>
     </>
   );
 }
