6e85bbcc05efd172720daf4227c8973e2f7622e7
ParkVitna <bbn960903@gmail.com>
2021-06-28 13:59:35 +0900

    [update] Inspection 01

--- src/views/Inspection/InspectionBarcodePop.js
+++ src/views/Inspection/InspectionBarcodePop.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./InspectionModal.css";
+import Barcode from "react-barcode";
 
 export const InspectionBarcodePop = (props) => {
   const {open, closeCheck, closeCancel, barcodeImg, inspectionListName, patientName, inspectionInspectorName} = props;
@@ -11,7 +12,9 @@ export const InspectionBarcodePop = (props) => {
         <section>
             {/* InspectionBarcodePop */}
           <div className="InspectionBarcodePop">
-            <div className="InspectionBarcodePop_1_1">
+          <div className="InspectionBarcodePop_title m-2">바코드 출력</div>
+          <div className="InspectionBarcodePop_1 border">
+            <div className="InspectionBarcodePop_1_1 m-3">
               <img src={`/resources/img/${barcodeImg}`} width="100%" height="100%" alt=""></img>
             </div>
             <div className="InspectionBarcodePop_1_2 border">
@@ -28,7 +31,8 @@ export const InspectionBarcodePop = (props) => {
                 <div>{inspectionInspectorName}</div>
               </div>
             </div>
-            <div className="InspectionBarcodePop_1_3">
+          </div>
+            <div className="InspectionBarcodePop_1_3 mb-3 mt-3">
               <button className="button_team2_fill" onClick={closeCheck}>확인</button>
               <button className="button_team2_empty" onClick={closeCancel}>취소</button>
             </div>
