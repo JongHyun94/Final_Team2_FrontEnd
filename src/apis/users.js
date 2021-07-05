0344cc6c395de6284f096c7fa383972c4c5f4218
seoyoung78 <tjdud0708@naver.com>
2021-07-05 22:28:07 +0900

    [update] user axios

--- src/apis/users.js
+++ src/apis/users.js
@@ -1,6 +1,23 @@
 import axios from "axios";
 
-export function getUserList(){
+export function getAllUserList(){
   const promise = axios.get("/user");
+  return promise;
+}
+
+export function getUserList(keyword="", authority=""){
+  console.log(authority);
+  const promise = axios.get("/user/select", {params: {keyword, authority}});
+  return promise;
+}
+
+export function updateUser(user) {
+  const promise = axios.put("/user", user);
+  return promise;
+}
+
+export function createUser(user) {
+  console.log(user);
+  const promise = axios.post("/user", user);
   return promise;
 }
