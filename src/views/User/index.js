ad898b73091b1ea04692ff2a0628081f1b252e71
JongHyun94 <ant9406@naver.com>
2021-07-21 15:48:36 +0900

    [update] mqtt url 설정

--- src/views/User/index.js
+++ src/views/User/index.js
@@ -57,7 +57,8 @@ function User(props) {
 
   const connectMqttBroker = () => {
     // Paho.Mqtt.Client x
-    client.current = new Paho.Client("localhost", 61614, "client-" + new Date().getTime());
+    client.current = new Paho.Client("kosa3.iptime.org", 61614, "client-" + new Date().getTime());
+    //client.current = new Paho.Client("localhost", 61614, "client-" + new Date().getTime());
 
     client.current.onConnectionLost = () => {
       console.log("Mqtt 접속 끊김");
